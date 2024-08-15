78306391527f623aa719d60f7050b32bd5a31181
admin <admin@DESKTOP-UL7CDI2>
2024-08-15 11:45:13 +0530

    first push

---
+++ jenkin_sselenium/src/test/java/jenkins/jenkinsTest.java
@@ -0,0 +1,15 @@
+package jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkinsTest {
+	@Test
+	public void launch() {
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("demowebshop launched",true);
+	}
+}
