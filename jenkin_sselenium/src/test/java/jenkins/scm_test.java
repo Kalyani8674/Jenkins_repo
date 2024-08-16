9c0ba509034e0cb3a2a08d68fa637c3837ee6e28
admin <admin@DESKTOP-UL7CDI2>
2024-08-16 11:58:25 +0530

    scm poll

---
+++ jenkin_sselenium/src/test/java/jenkins/scm_test.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class scm_test {
+	@Test
+	public void pollscm() {
+		Reporter.log("scm added ,execution");
+	}
+}
